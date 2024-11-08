db39a272d6ddde48bab538b8db7aed1a458724f6
Anil Kaya <kaya@iff031.iff.kfa-juelich.de>
2024-11-08 13:50:13 +0000

    algorithm selection is implemented on run_shd.py

--- run_shd.py
+++ run_shd.py
@@ -2,7 +2,6 @@ from functools import partial
 import argparse
 # import wandb
 import torch
-import yaml
 from tqdm import tqdm
 
 import jax
@@ -10,6 +9,8 @@ import jax.nn as jnn
 import jax.lax as lax
 import jax.numpy as jnp
 import jax.random as jrand
+import jax.nn.initializers as initializers
+import jax.profiler as profiler
 
 import optax
 
@@ -18,13 +19,15 @@ from synaptax.experiments.shd.bptt import make_bptt_step, make_bptt_rec_step
 from synaptax.experiments.shd.eprop import make_eprop_step, make_eprop_rec_step
 from synaptax.custom_dataloaders import load_shd_or_ssc
 
-# jax.config.update("jax_disable_jit", True)
+import yaml
+import wandb
+
+#jax.config.update("jax_disable_jit", True)
 
 parser = argparse.ArgumentParser()
 
 parser.add_argument("-d", "--path", default="./data/shd", type=str, help="Path to the dataset.")
-parser.add_argument("-c", "--config", default="./src/synaptax/experiments/shd/config/params.yaml", 
-                    type=str, help="Path to the configuration yaml file.")
+parser.add_argument("-c", "--config", default="./config/params.yaml", type=str, help="Path to the configuration yaml file.")
 parser.add_argument("-s", "--seed", default=0, type=int, help="Random seed.")
 parser.add_argument("-e", "--epochs", default=100, type=int, help="Number of epochs.")
 
@@ -37,32 +40,40 @@ torch.manual_seed(SEED)
 with open(args.config, "r") as file:
     config_dict = yaml.safe_load(file)
 
-NEURON_MODEL = str(config_dict["neuron_model"])
-LEARNING_RATE = float(config_dict["hyperparameters"]["learning_rate"])
-BATCH_SIZE = int(config_dict["hyperparameters"]["batch_size"])
-NUM_TIMESTEPS = int(config_dict["hyperparameters"]["timesteps"])
+neuron_model_dict = {
+    "SNN_LIF": SNN_LIF,
+    "SNN_rec_LIF": SNN_rec_LIF,
+    "SNN_Sigma_Delta": SNN_Sigma_Delta
+}
+
+NEURON_MODEL = str(config_dict['neuron_model'])
+LEARNING_RATE = float(config_dict['hyperparameters']['learning_rate'])
+BATCH_SIZE = int(config_dict['hyperparameters']['batch_size'])
+NUM_TIMESTEPS = int(config_dict['hyperparameters']['timesteps'])
 EPOCHS = args.epochs
-NUM_HIDDEN = int(config_dict["hyperparameters"]["hidden"])
-PATH = str(config_dict["dataset"]["folder_path"])
-NUM_WORKERS = int(config_dict["dataset"]["num_workers"])
+NUM_HIDDEN = int(config_dict['hyperparameters']['hidden'])
+PATH = str(config_dict['dataset']['folder_path'])
+NUM_WORKERS = int(config_dict['dataset']['num_workers'])
 NUM_LABELS = 20
 NUM_CHANNELS = 700
-BURNIN_STEPS = config_dict["hyperparameters"]["burnin_steps"]
+BURNIN_STEPS = int(config_dict['hyperparameters']['burnin_steps'])
+LOOP_UNROLL = int(config_dict['hyperparameters']['loop_unroll'])
+TRAIN_ALGORITHM = str(config_dict['train_algorithm'])
 
-"""
+'''
 # Initialize wandb:
 wandb.login()
 
 run = wandb.init(
     # Set the project where this run will be logged
-    project=config_dict["task"],
+    project=config_dict['task'],
     # Track hyperparameters and run metadata
     config={
         "learning_rate": LEARNING_RATE,
         "epochs": EPOCHS,
     },
 )
-"""
+'''
 
 train_loader = load_shd_or_ssc("shd", PATH, "train", BATCH_SIZE, 
                                 nb_steps=NUM_TIMESTEPS, shuffle=True,
@@ -70,7 +81,6 @@ train_loader = load_shd_or_ssc("shd", PATH, "train", BATCH_SIZE,
 test_loader = load_shd_or_ssc("shd", PATH, "test", BATCH_SIZE, 
                                 nb_steps=NUM_TIMESTEPS, shuffle=True,
                                 workers=NUM_WORKERS)
-
 
 # Cross-entropy loss
 def ce_loss(z, tgt, W_out):
@@ -83,31 +93,32 @@ def predict(in_seq, model, weights, z0, u0):
     W_out = weights[-1]
     Ws = weights[:-1]
     def loop_fn(carry, x):
-        z, u, out_total = carry
+        z, u, z_total = carry
         z_next, u_next = model(x, z, u, *Ws)
-        out_total += jnp.dot(W_out, z_next)
-        carry = (z_next, u_next, out_total)
+        z_total += jnp.dot(W_out, z_next)
+        carry = (z_next, u_next, z_total)
         return carry, None
 
     final_carry, _ = lax.scan(loop_fn, (z0, u0, jnp.zeros(NUM_LABELS)), in_seq)
     out = final_carry[2]
+    # probs = jax.nn.softmax(out) # not necessary to use softmax here
     return jnp.argmax(out, axis=0)
 
 
 # Test for one batch:
 @partial(jax.jit, static_argnums=2)
-def eval_step(in_batch, labels, model, weights, z0, u0):
+def eval_step(in_batch, target_batch, model, weights, z0, u0):
     preds_batch = jax.vmap(predict, in_axes=(0, None, None, None, None))(in_batch, model, weights, z0, u0)
-    return (preds_batch == labels).mean()
+    return (preds_batch == target_batch).mean()
 
 
 # Test loop
 def eval_model(data_loader, model, weights, z0, u0):
     accuracy_batch, num_iters = 0, 0
-    for data, labels, lengths in data_loader:
+    for data, target_batch, lengths in data_loader:
         in_batch = jnp.array(data.numpy()).squeeze()
-        labels = jnp.array(labels.numpy())
-        accuracy_batch += eval_step(in_batch, labels, model, weights, z0, u0)
+        target_batch = jnp.array(target_batch.numpy())
+        accuracy_batch += eval_step(in_batch, target_batch, model, weights, z0, u0)
         num_iters += 1
     return accuracy_batch / num_iters
 
@@ -118,12 +129,13 @@ u0 = jnp.zeros(NUM_HIDDEN)
 
 wkey, woutkey = jrand.split(key, 2)
 
-init_fn = jnn.initializers.orthogonal(jnp.sqrt(2))
-W_out_init_fn = jnn.initializers.xavier_normal()
+init_fn_xavier = initializers.xavier_normal()
 
-W = init_fn(wkey, (NUM_HIDDEN, NUM_CHANNELS))
+init_fn_orthogonal = initializers.orthogonal(jnp.sqrt(2))
+
+W = init_fn_orthogonal(wkey, (NUM_HIDDEN, NUM_CHANNELS))
 V = jnp.zeros((NUM_HIDDEN, NUM_HIDDEN))
-W_out = W_out_init_fn(woutkey, (NUM_LABELS, NUM_HIDDEN))
+W_out = init_fn_xavier(woutkey, (NUM_LABELS, NUM_HIDDEN)) # initialzie with xavier normal.
 
 G_W0 = jnp.zeros((NUM_HIDDEN, NUM_CHANNELS))
 G_V0 = jnp.zeros((NUM_HIDDEN, NUM_HIDDEN))
@@ -131,33 +143,76 @@ W_out0 = jnp.zeros((NUM_LABELS, NUM_HIDDEN))
 
 optim = optax.chain(optax.adamw(LEARNING_RATE, eps=1e-7, weight_decay=1e-3), 
                     optax.clip_by_global_norm(.5))
-# weights = (W, V, W_out) # For recurrence
-weights = (W, W_out) # For no recurrence
-opt_state = optim.init(weights)
-model = SNN_LIF
-step_fn = make_eprop_step(model, optim, ce_loss, unroll=10, burnin_steps=BURNIN_STEPS)
-# step_fn = make_eprop_rec_step(model, optim, ce_loss, unroll=10)
-# step_fn = make_bptt_step(model, optim, ce_loss, unroll=10, burnin_steps=BURNIN_STEPS)
-# step_fn = make_bptt_rec_step(model, optim, ce_loss, unroll=10)
 
-# jax.config.update("jax_disable_jit", True)
+model = neuron_model_dict[NEURON_MODEL]
 
 # Training loop
-for ep in range(EPOCHS):
-    pbar = tqdm(train_loader)
-    for data, labels, lengths in pbar:
-        data = jnp.array(data.numpy()).squeeze()
-        labels = jnp.array(labels.numpy())
-        labels = jnn.one_hot(labels, NUM_LABELS)
+def run_(partial_step_fn, weights, opt_state):
+    for ep in range(EPOCHS):
+        pbar = tqdm(train_loader)
+        for data, target_batch, lengths in pbar:
+            in_batch = jnp.array(data.numpy()).squeeze()
+            target_batch = jnp.array(target_batch.numpy())
+            target_batch = jnn.one_hot(target_batch, NUM_LABELS)
 
-        # just comment out "bptt" with "eprop" to switch between the two training methods
-        # With e-prop:
-        loss, weights, opt_state = step_fn(data, labels, opt_state, weights, z0, u0, G_W0, W_out0)
-        # With bptt:
-        # loss, weights, opt_state = step_fn(in_batch, labels, opt_state, weights, z0, u0)
-        pbar.set_description(f"Epoch: {ep + 1}, loss: {loss.mean() / NUM_TIMESTEPS}")
+            loss, weights, opt_state = partial_step_fn(in_batch=in_batch, 
+                                                       weights=weights, 
+                                                       target_batch=target_batch, 
+                                                       opt_state=opt_state)
+            
+            pbar.set_description(f"Epoch: {ep + 1}, loss: {loss.mean() / NUM_TIMESTEPS}")
+        
+        train_acc = eval_model(train_loader, model, weights, z0, u0)
+        test_acc = eval_model(test_loader, model, weights, z0, u0)
+        print(f"Epoch: {ep + 1}, Train Acc: {train_acc}, Test Acc: {test_acc}")
     
-    train_acc = eval_model(train_loader, model, weights, z0, u0)
-    test_acc = eval_model(test_loader, model, weights, z0, u0)
-    print(f"Epoch: {ep + 1}, Train Acc: {train_acc}, Test Acc: {test_acc}")
-    
+    return weights
+        
+
+def run_eprop():
+    weights = (W, W_out) # For non-recurrent case.
+    opt_state = optim.init(weights)
+    step_fn = make_eprop_step(model, optim, ce_loss, 
+                              unroll=LOOP_UNROLL, burnin_steps=BURNIN_STEPS)
+    partial_step_fn = partial(step_fn, z0=z0, u0=u0, G_W0=G_W0, W_out0=W_out0)
+    trained_weights = run_(partial_step_fn, weights, opt_state)
+    return trained_weights
+
+def run_eprop_rec():
+    weights = (W, V, W_out) # For recurrent case.
+    opt_state = optim.init(weights)
+    step_fn = make_eprop_rec_step(model, optim, ce_loss, 
+                                  unroll=LOOP_UNROLL, burnin_steps=BURNIN_STEPS)
+    partial_step_fn = partial(step_fn, z0=z0, u0=u0, G_W0=G_W0, G_V0=G_V0, W_out0=W_out0)
+    trained_weights = run_(partial_step_fn, weights, opt_state)
+    return trained_weights
+
+def run_bptt():
+    weights = (W, W_out) # For non-recurrent case.
+    opt_state = optim.init(weights)
+    step_fn = make_bptt_step(model, optim, ce_loss, 
+                              unroll=LOOP_UNROLL, burnin_steps=BURNIN_STEPS)
+    partial_step_fn = partial(step_fn, z0=z0, u0=u0)
+    trained_weights = run_(partial_step_fn, weights, opt_state)
+    return trained_weights
+
+def run_bptt_rec():
+    weights = (W, V, W_out) # For recurrent case.
+    opt_state = optim.init(weights)
+    step_fn = make_bptt_rec_step(model, optim, ce_loss, 
+                              unroll=LOOP_UNROLL, burnin_steps=BURNIN_STEPS)
+    partial_step_fn = partial(step_fn, z0=z0, u0=u0)
+    trained_weights = run_(partial_step_fn, weights, opt_state)
+    return trained_weights
+
+train_algo_dict = {
+    "eprop": run_eprop,
+    "eprop_rec": run_eprop_rec,
+    "bptt": run_bptt,
+    "bptt_rec": run_bptt_rec
+}
+
+#profiler.start_trace("/tmp/tensorboard")
+new_weights = train_algo_dict[TRAIN_ALGORITHM]()
+#new_weights.block_until_ready()
+#profiler.stop_trace()
