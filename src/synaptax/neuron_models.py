0db0b46981e56fe786091135ebc0c54a1534983a
Anil Kaya <kaya@iff031.iff.kfa-juelich.de>
2024-11-08 14:02:02 +0000

    neuron threshold in surrogate function are fixed

--- src/synaptax/neuron_models.py
+++ src/synaptax/neuron_models.py
@@ -16,12 +16,12 @@ def SNN_LIF(x, z, u, W):
     W: forward weights.
     """
     # beta = 0.95
-    # threshold = 1.
+    threshold = 1.
     u_next = .95 * u + jnp.dot(W, x) # (1. - beta) * 
-    surr = surrogate(u_next-1.)
+    surr = surrogate(u_next-threshold)
     # Trick so that in forward pass we get the heaviside spike output and in
     # backward pass we get the derivative of surrogate only without heaviside.
-    z_next = lax.stop_gradient(jnp.heaviside(u_next - 1., 0.) - surr) + surr
+    z_next = lax.stop_gradient(jnp.heaviside(u_next-threshold, 0.) - surr) + surr
     u_next -= lax.stop_gradient(z_next)
     return z_next, u_next
 
@@ -38,10 +38,10 @@ def SNN_rec_LIF(x, z, u, W, V):
     beta = 0.95 # seems to work best with this value
     threshold = 1.
     u_next = beta * u + jnp.dot(W, x) + jnp.dot(V, z)
-    surr = surrogate(u_next)
+    surr = surrogate(u_next-threshold)
     # Trick so that in forward pass we get the heaviside spike output and in
     # backward pass we get the derivative of surrogate only without heaviside.
-    z_next = lax.stop_gradient(jnp.heaviside(u_next - threshold, 0.) - surr) + surr
+    z_next = lax.stop_gradient(jnp.heaviside(u_next-threshold, 0.) - surr) + surr
     u_next -= z_next * threshold
     return z_next, u_next
 
@@ -57,10 +57,10 @@ def SNN_rec_LIF_Stopgrad(x, z, u, W, V):
     beta = 0.95 # seems to work best with this value
     threshold = 1.
     u_next = beta * u + (1. - beta) * (jnp.dot(W, x)+ lax.stop_gradient(jnp.dot(V, z)))
-    surr = surrogate(u_next)
+    surr = surrogate(u_next-threshold)
     # Trick so that in forward pass we get the heaviside spike output and in
     # backward pass we get the derivative of surrogate only without heaviside.
-    z_next = lax.stop_gradient(jnp.heaviside(u_next - threshold, 0.) - surr) + surr
+    z_next = lax.stop_gradient(jnp.heaviside(u_next-threshold, 0.) - surr) + surr
     u_next -= z_next * threshold
     return z_next, u_next
 
@@ -69,14 +69,14 @@ def SNN_Sigma_Delta(in_, z, e, u_mem, s, i, W, V):
     input_decay = .95
     membrane_decay = .95
     feedback_decay = .95
-    threshold_ = .0
+    threshold = 1.
 
     act_ = jnp.dot(W, in_) + jnp.dot(V, z)
     i = i * input_decay + act_
     e = i - s
     u_mem = u_mem * membrane_decay + e
     surr_ = surrogate(u_mem - threshold_)
-    z_out = jax.lax.stop_gradient(jnp.heaviside(u_mem - threshold_, .0) - surr_) + surr_
+    z_out = jax.lax.stop_gradient(jnp.heaviside(u_mem-threshold, .0) - surr_) + surr_
     s = s * feedback_decay + z_out
     return z_out, e, u_mem, s, i
 
